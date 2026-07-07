555c2b47d93fdbc88f6589ea370345ca3d364c74
berik <berik@MacBook-Pro-berik.local>
2026-07-07 16:04:29 +0500

    fix 2

--- scripts/product-validation-condition.php
+++ scripts/product-validation-condition.php
@@ -5,7 +5,7 @@ $stock = 5;
 $isActive = true;
 
 
-if ($name != "") {
+if ($name !== "") {
     echo "Name is not empty" . "\n";
 } else {
     echo "Name is empty" . "\n";
@@ -14,16 +14,18 @@ if ($name != "") {
 if ($price > 0) {
     echo "Price is positive" . "\n";
 } else {
-    echo "Price is negative" . "\n";
+    echo "Price must be greater than zero" . "\n";
 }
 
 if ($stock >= 0) {
     echo "Stock is positive" . "\n";
 } else {
-    echo "Stock is negative" . "\n";
+    echo "Price must be greater than zero" . "\n";
 }
-if ($isActive === true && $stock >= 0) {
+if ($isActive === true && $stock > 0) {
     echo "Product is active and has stock" . "\n";
+} else {
+    echo "Product is not active or has no stock" . "\n";
 }
 
 $name = "";
@@ -31,5 +33,26 @@ if ($name != "") {
     echo "Name is not empty" . "\n";
 } else {
     echo "Name is empty" . "\n";
+}
+
+$price = -1;
+if ($price > 0) {
+    echo "Price is positive" . "\n";
+} else {
+    echo "Price must be greater than zero" . "\n";
+}
+
+$stock = -1;
+if ($stock >= 0) {
+    echo "Stock is positive" . "\n";
+} else {
+    echo "Price must be greater than zero" . "\n";
+}
+
+$isActive = false;
+if ($isActive === true && $stock > 0) {
+    echo "Product is active and has stock" . "\n";
+} else {
+    echo "Product is not active or has no stock" . "\n";
 }
 
