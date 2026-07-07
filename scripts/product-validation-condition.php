5bebe1502bfd0c0378e0dfb6a8d40dd0248cd7c0
berik <berik@MacBook-Pro-berik.local>
2026-07-07 15:54:18 +0500

    fix homework

--- scripts/product-validation-condition.php
+++ scripts/product-validation-condition.php
@@ -7,16 +7,22 @@ $isActive = true;
 
 if ($name != "") {
     echo "Name is not empty" . "\n";
+} else {
+    echo "Name is empty" . "\n";
 }
 
 if ($price > 0) {
     echo "Price is positive" . "\n";
+} else {
+    echo "Price is negative" . "\n";
 }
 
-if ($stock > 0) {
+if ($stock >= 0) {
     echo "Stock is positive" . "\n";
+} else {
+    echo "Stock is negative" . "\n";
 }
-if ($isActive === true && $stock > 0) {
+if ($isActive === true && $stock >= 0) {
     echo "Product is active and has stock" . "\n";
 }
 
